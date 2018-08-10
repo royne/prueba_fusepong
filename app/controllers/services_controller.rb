ca4f0be98da164b76fce77ef4d58cdba65433e0e
royne <royne.chaves@gmail.com>
2018-08-10 16:30:45 -0500

    asigna roles

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,4 +1,6 @@
 class ServicesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @services = Service.all
   end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,6 @@
 class Service < ApplicationRecord
   validates :origin, :destination, presence: true
+  resourcify
 
   belongs_to :user
 end

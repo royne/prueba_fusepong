a877b4ea80bd277ecf47293847562e30f0548d45
German Lopez <9410ger@gmail.com>
2018-08-14 15:07:49 -0500

    asignacion de mensajeros ok

--- app/controllers/asignations_controller.rb
+++ app/controllers/asignations_controller.rb
@@ -1,17 +1,17 @@
 class AsignationsController < ApplicationController
   def new
+    @service = params[:service]
     @asignation = Asignation.new
   end
 
   def create
-    @service = Service.find(params[:id])
-    # byebug
-    @asignation = Asignation.create(asignation_params)
+    binding.pry
+    asignation = Asignation.create(asignation_params)
 
-    if @asignation.save
+    if asignation.save
       redirect_to services_path, notice:"El mensajero se ha asignado con exito"
     else
-      flash[:alert] = @asignation.errors.full_messages
+      flash[:alert] = asignation.errors.full_messages
       render :new
     end
   end

ccbfc23bd0458e04ee72f0f520b10ee8fcb31f0f
German Lopez <9410ger@gmail.com>
2018-08-15 13:57:47 -0500

    restriccion de vistasa mensajeros

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -5,6 +5,10 @@ class ServicesController < ApplicationController
     @services = Service.all
     @asignations = Asignation.all
     @user_message = User.all
+
+    if current_user.has_role? :message
+      redirect_to message_path(current_user.id)
+    end
   end
 
   def new
@@ -23,10 +27,19 @@ class ServicesController < ApplicationController
 
   def show
     @service = Service.find(params[:id])
+    restriction_views
   end
 
   private
     def services_params
       params.require(:service).permit(:origin, :destination, :date, :time)
+    end
+
+    def restriction_views
+      if current_user.has_role? :client
+        redirect_to root_path
+      elsif current_user.has_role? :message
+        redirect_to message_path(current_user.id)
+      end
     end
 end
